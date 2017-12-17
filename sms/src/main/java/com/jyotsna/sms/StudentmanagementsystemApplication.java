a35f492ac4aed484a2a7c331c12d35e4b66bc8ed
Anil Kumar T <anilt@xavient.com>
2017-12-17 22:39:22 +0530

    This is my first commit to the project

---
+++ sms/src/main/java/com/jyotsna/sms/StudentmanagementsystemApplication.java
@@ -0,0 +1,12 @@
+package com.jyotsna.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementsystemApplication.class, args);
+	}
+}
