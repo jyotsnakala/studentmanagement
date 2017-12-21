2c1ef1e2deafc5f2bd32f0a88b5a23bc1ffe64c2
Anil Kumar T <anilt@xavient.com>
2017-12-21 20:16:51 +0530

    testing

--- sms/src/main/java/com/jyotsna/sms/StudentmanagementsystemApplication.java
+++ sms/src/main/java/com/jyotsna/sms/StudentmanagementsystemApplication.java
@@ -8,5 +8,6 @@ public class StudentmanagementsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementsystemApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
